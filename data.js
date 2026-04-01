43f904d7812e927d5c05a611684e886d2135fb9b
Fikri Arrayan <arrayan407@gmail.com>
2026-04-01 11:49:31 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  { nama: "budi", umur: "21", alamat: "jln bambu" },
+  { nama: "andi", umur: "42", alamat: "jln kuningan" },
+  { nama: "daya", umur: "24", alamat: "jln cijanteng" },
+  { nama: "ramzi", umur: "28", alamat: "jln margonda" },
+  { nama: "hendra", umur: "20", alamat: "jln kalibaru" },
+  { nama: "hartono", umur: "31", alamat: "jln cacing" },
+  { nama: "rusdi", umur: "35", alamat: "jln ngawi" },
+  { nama: "farhan", umur: "30", alamat: "jln teluk telaga" },
+  { nama: "fajar", umur: "21", alamat: "jln ancol barat" },
+  { nama: "tini", umur: "22", alamat: "jln warakas" },
+];
+
+export default data;
