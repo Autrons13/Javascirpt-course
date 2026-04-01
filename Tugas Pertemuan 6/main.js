43ece04b5eb6e7f669c27a21d0a4637e953d98f3
Fikri Arrayan <arrayan407@gmail.com>
2026-04-01 11:50:38 +0700

    Add files via upload

---
+++ Tugas Pertemuan 6/main.js
@@ -0,0 +1,12 @@
+import { lihatData, tambahData, hapusData } from "./controller.js";
+
+// tampilan data awal
+lihatData();
+// data ditambahkan
+tambahData();
+// tampilan data setelah ditambahkan
+lihatData();
+// data dihapus
+hapusData(0);
+// tampilan data setelah dihapus
+lihatData();
